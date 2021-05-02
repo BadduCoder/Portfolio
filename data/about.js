59db2f8bfa2445c7d8418430b7e1485935bed3cb
BadduCoder <sumit7engineer@gmail.com>
2021-05-02 23:24:22 +0530

    Populate experience, project data

---
+++ data/about.js
@@ -0,0 +1,6 @@
+const about_data = {
+    "Name" : "Sumit Badsara",
+    "College" : "NIT Raipur",
+    "Graduation Year" : 2021,
+    "CGPA" : 8.34,
+}
--- data/experiences.js
+++ data/experiences.js
@@ -1,11 +1,34 @@
 const experience_data = [
     {
-        "organisation-name":"FamPay",
-        "role":"Backend Intern",
-        "description" : [
-            "First Point",
-            "Second Point",
-            "Third Point"
+        "Organisation Name":"FamPay",
+        "Role":"Backend Intern",
+        "Start Date":"OCT, 2020",
+        "End Date":"PRESENT",
+        "Description" : [
+            "Integrated bank APIs to reduce human efforts and automate recon process.",
+            "Worked on major features such as stories, new contextual card, new reward type.",
+            "Worked as Point Of Contact from technical team for customer success team and resolved queries/bugs."
+        ]
+    },
+    {
+        "Organisation Name":"GE Healthcare",
+        "Role":"EID Intern",
+        "Start Date":"JUN, 2020",
+        "End Date":"AUG, 2020",
+        "Description" : [
+            "Achieved 85%+ coverage in tests created for MyGEHC Mobile Application",
+            "Improved and added around 100+ unit tests.",
+            "Minor contributions in GraphQL resolvers for REST APIs."
+        ]
+    },
+    {
+        "Organisation Name":"Zenithec Techware",
+        "Role":"Software Developer Intern",
+        "Start Date":"DEC, 2019",
+        "End Date":"APR, 2020",
+        "Description" : [
+            "Handled production and deployment of 2 subdomains.",
+            "Explored 5+ methods of Pdf generation in Javascript, Php, Python."
         ]
     }
 ]
